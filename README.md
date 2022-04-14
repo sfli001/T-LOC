c5afcb181b1b59397149cf6a0d62e249c316d5f7
SL <sfli001@gmail.com>
2022-04-14 17:15:51 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,147 @@
+## T-LOC: A T-DNA insertion sites Locator
+A program using whole-genome sequencing data to localize T-DNA insertion site in plant genome and output the diagram of T-DNA insertion site, mosaic sequence reads, flanking sequences of T-DNA insertion sites as well as the coverage of  TDNA insertion sites flanking region.
+The program were based on the mosaic reads aligned to both reference and TDNA. Meanwhile, the program also output mosaic reads aligned to two difference position at the reference genomes as well as two difference positions at the TDNA.
+
+Requirements
+------------
+1. Install [Python 2.7.x](https://www.python.org/downloads)
+2. Install [bwa](http://bio-bwa.sourceforge.net/)
+3. Install [R](https://www.r-project.org)
+4. Install [ShortRead](http://www.bioconductor.org/packages/release/bioc/html/ShortRead.html)
+
+Installation
+------------
+The source code can be directly called from Python.
+
+Example Usage
+--------------------------------
+Run T-LOC with: sequenced fastq files with T-DNA insertion, genome fasta files,  TDNA fasta files, 
+
+        python T-LOC.py  --fastq samples_1.fq.gz,samples_2.fq.gz --genome Reference.fa  --TDNA TDNA.fa --ouput outputfolder
+
+Run T-LOC with: bam files alignment to both reference and TDNA, genome fasta files,  TDNA fasta files,
+
+        python T-LOC.py  --bamR samples_Ref.bam --bamT samples_TDNA.bam --genome Reference.fa  --TDNA TDNA.fa --ouput outputfolder
+
+Run T-LOC with: sequenced fastq files with T-DNA insertion,  sequenced fastq files without T-DNA insertion as control, genome fasta files,  TDNA fasta files
+
+        python T-LOC.py  --fastq samples_1.fq.gz,samples_2.fq.gz --genome Reference.fa  --TDNA TDNA.fa --control_fastq control_s1_1.fq.gz,control_s1_2.fq.gz:control_s2_1.fq.gz,control_s2_2.fq.gz --ouput outputfolder
+
+Run T-LOC with: sequenced fastq files with T-DNA insertion,  bam files alignment to reference without T-DNA insertion as control, genome fasta files,  TDNA fasta files
+
+        python T-LOC.py  --fastq samples_1.fq.gz,samples_2.fq.gz --genome Reference.fa  --TDNA TDNA.fa --control_bamR control_s1.sam,control_s2.sam --ouput outputfolder
+
+
+Required Parameters
+------------
+	--genome:
+		The fasta files of the reference genome
+	--TDNA:
+		The fasta files of the TDNA  genome
+	--fastq: 
+		s1_1.fq.gz[,s1_2.fq.gz]. The raw sequencing reads in fastq format from plant with TDNA insertions that are used to align to the reference genome and TDNA genomes. It can be omitted if both bamR and bamT were provided.
+	--bamR:
+		Sorted bam file alignment to the reference genome from sequencing reads on plant with TDNA insertions. It can be omitted if fastq files  were provided.
+	--bamT:
+                Sorted Bam file alignment to the TDNA genome from sequencing reads on plant with TDNA insertions. It can be omitted if fastq files  were provided.
+
+Optional Parameters
+------------	
+	--control_fastq:
+		c1_1.fq.gz[,c1_2.fq.gz][:c2_1.fq.gz[,c2_2.fq.gz],...]. The raw sequencing reads in fastq format from plant without TDNA insertions that are used to align to the reference genome to produce control clipped read ID
+	--control_bamR:
+		c1.bam[,c2.bam,...]. Bam/sam file alignment to the reference genome from sequencing reads on plant without TDNA insertions that are used to produce control clipped read ID
+	--control_ID:
+		A txt file that are clipped read ID from plant without TDNA insertions. If control_fastq, control_bamR and control_ID were all missed, TDNA insertion sites were localized through TDNA clipped reads only.
+	--anchor:
+		The minimal anchor length required for the clipped reads. The default value is 20.
+	--insert:
+		The maximal insertion sequence length at the TDNA insertion sites. The default value is 30.
+	--genome_Bwa: 
+		bwa index of given reference genome
+	--TDNA_Bwa:    
+                bwa index of given TDNA sequence
+	--read_min_TDNA:
+		The minimal number of mosaic reads required for each TDNA insertion sites as well as mosaic reads between two TDNA positions. The default value is 2. It can be set to any integer larger or equal to 2. 
+	--read_min_REF:
+		The minimal number of mosaic reads bwteen two reference positions. The default value is 4. It can be set to any integer larger or equal to 2. If the program run slow, please increase the number.
+	--Sample_name:
+		sample names will be used to label output files,defalut is ss
+	--resume:
+		 Whether to resume previous run. The default is true
+	--Mosaic_length: 
+		the length of output sequences of each mosaic side
+	--ouptut, -o
+                The output folder. The default is current directory
+ 
+
+
+Output list
+--------------------------------
+Result
+
+	The folder contains the final output three types of files: 
+	MosaicFromTDNA.pdf
+		It contains the TDNA insertion diagram and mosaic reads at both sides for each of the TDNA insertion sites calucated from TDNA clipped alignment reads.	i
+	MosaicFromREF.pdf
+       		It contains the TDNA insertion diagram and mosaic reads at both sides for each of the TDNA insertion sites calucated from reference clipped alignment reads.
+	MosaicFromTDNA.fasta
+		It contains the flanking reference sequences as well as the TDNA sequence for each of the TDNA insertion sites calucated from TDNA clipped alignment reads.
+	MosaicFromREF.fasta
+       		It contains the flanking reference sequences as well as the TDNA sequence for each of the TDNA insertion sites calucated from reference clipped alignment reads. 
+	CovREF.pdf
+		The coverage of reference genome for each of these flanking regions of T-DNA insertion sites.
+
+log.T-LOC
+
+	Log file for running T-LOC pipeline
+
+Bwa_genome
+
+	The folder contains the bwa index files of reference genome
+
+TDNA_genome
+
+	The folder contains the bwa index files of TDNA sequences
+
+REF
+
+	The folder contains the middle files using reference clipped reads
+
+TDNA
+
+	The folder contains the middle files using TDNA clipped reads
+
+Control
+
+	The folder contains the middle files to generate control clip ID from plants without TDNA insertions
+
+
+Contacts and bug reports
+------------------------
+
+Shaofang Li
+shaofangli2021@hotmail.com
+
+If you found a bug or mistake in this project, we would like to know about it.
+Before you send us the bug report though, please check the following:
+
+1. Are you using the latest version? The bug you found may already have been
+   fixed.
+2. Check that your input is in the correct format and you have selected the
+   correct options.
+3. Please reduce your input to the smallest possible size that still produces
+   the bug; we will need your input data to reproduce the problem, and the
+   smaller you can make it, the easier it will be.
+
+
+Copyright and License Information
+---------------------------------
+Copyright (C) 2021, Shaofang Li
+
+Authors: Shaofang Li
+
+A patent has been applied for this program. For commercial users, please contact me at shaofangli2021@hotmail.com.
+This program is free for academic users. For publication, please cite "T-LOC: a comprehensive tool to localize and characterize the T-DNA integration sites", Li et al 2022 Plant Physiology.
+
+
---
+++ T-LOC.py
@@ -0,0 +1,298 @@
+#!/bin/python
+import getopt,copy,re,os,sys,logging,time,datetime;
+options, args = getopt.getopt(sys.argv[1:], 'o:',['fastq=','bamR=','bamT=','genome=','TDNA=','genome_Bwa=','TDNA_Bwa=','anchor=','output=','resume=', 'read_min_TDNA=','read_min_REF=','control_bamR=','insert=','control_fastq=', 'control_ID=', 'Mosaic_length=','Sample_name='])
+fastq = ""
+bamR=""
+control_bamR=""
+control_fastq=""
+control_ID=""
+bamT=""
+genome=""
+genome_Bwa=""
+TDNA=""
+TDNA_Bwa=""
+anchor = 20
+output = "./"
+resume = "true"
+read_min_TDNA = 2
+read_min_REF = 4
+insert = 30
+clipped_samR = ""
+clipped_samT = ""
+Mosaic_length = 500
+Sample_name = 'ss'
+#bam="/home/shaofangli/OFF-target/DNA/46bAG_s1.sorted.bam"
+#clipped_sam="/home/shaofangli/OFF-target/DNA/TDNA_mosaic/46bAG_s1_clipped.sam"
+#genome="/home/shaofangli/genome/rice/Kitaake/OsativaKitaake_499_v3.0.fa"
+#genome_Bwa="/home/shaofangli/genome/rice/Kitaake/Bwaindex/OsativaKitaake_499_v3.0.fa"
+#TDNA= "/home/shaofangli/OFF-target/TDNA/Bwa/V46bAG.fa"
+#TDNA_Bwa = "/home/shaofangli/OFF-target/TDNA/Bwa/V46bAG.fa"
+#LB= "2375:2399"
+#RB="9994:10018"
+#anchor = 30
+#output = "/home/shaofangli/SOFT/T_LOC/test/46bAG"
+#full programs from the begining require fastq, control_fastq, Reference fasta, TDNA fasta
+##full programs from the bwa alignment files requires 
+for opt, arg in options:
+	if opt in ('--fastq'):
+		fastq = arg
+        elif opt in ('--control_bamR'):
+                control_bamR = arg
+        elif opt in ('--control_fastq'):
+                control_fastq = arg
+        elif opt in ('--control_ID'):
+                control_ID = arg
+	elif opt in ('--bamR'):
+                bamR = arg
+        elif opt in ('--bamT'):
+                bamT = arg
+	elif opt in ('--genome'):
+               	genome = arg
+	elif opt in ('--TDNA'):
+                TDNA = arg
+	elif opt in ('--genome_Bwa'):
+                genome_Bwa = arg
+        elif opt in ('--TDNA_Bwa'):
+                TDNA_Bwa = arg
+	elif opt in ('--anchor'):
+                anchor = int(arg)
+        elif opt in ('--insert'):
+                insert = int(arg)
+        elif opt in ('--read_min_TDNA'):
+                read_min_TDNA = int(arg)
+        elif opt in ('--read_min_REF'):
+                read_min_REF = int(arg)
+        elif opt in ('--Mosaic_length'):
+                Mosaic_length = int(arg)
+	elif opt in ('--resume'):
+                resume  = arg
+        elif opt in ('--Sample_name'):
+                Sample_name  = arg
+        elif opt in ('-o','--output'):
+                output = arg
+run = "true"
+if(not fastq):
+        if( not bamR ):
+                print "Please provide either fastq reads or reference alignment files"
+                run ="false"
+	if( not bamT ):
+                print "Please provide either fastq reads or TDNA alignment files"
+                run ="false"
+if(not genome or not TDNA or not output):
+	print "please provide the reference genome sequence and TDNA sequences and fastq reads"
+	run = "false"
+
+if (run =="false"):
+	print "Not enough parameters!"
+	print "Program : ", sys.argv[0]
+	print "          A python program to locate the T_DNA insertion site.\n"
+        print "Usage :", sys.argv[0], " Required Parameters;"
+	print "Usage :", sys.argv[0], " --fastq: the sequence fastq files, paired reads were separated by comma;"
+	print "Usage :", sys.argv[0], " --bamR: sorted bam files of alignment to reference genome;"
+        print "Usage :", sys.argv[0], " --bamT: sorted bam files of TDNA alignment;"
+	print "Usage :", sys.argv[0], " --genome: fasta format of given genome sequences;"
+	print "Usage :", sys.argv[0], " --TDNA: fasta format of given T-DNA sequences;"
+        print "Usage :", sys.argv[0], " Optional  Parameters;"
+        print "Usage :", sys.argv[0], " --control_bamR: a series of bam files from plants without T-DNA insertion, different bam files were separated by comma;"
+        print "Usage :", sys.argv[0], " --control_fastq: a series of fastq files from plants without T-DNA insertion, different fastq files were sperated by colon, paired reads were separated by comma;"
+        print "Usage :", sys.argv[0], " --control_ID: the control ID files;"
+        print "Usage :", sys.argv[0], " --genome_Bwa: bwa index of given reference genome;"
+        print "Usage :", sys.argv[0], " --TDNA_Bwa: bwa index of given T-DNA sequences;"
+	print "Usage :", sys.argv[0], " --anchor: The minimal anchor length required for the clipped reads. The default value is 20;"
+        print "Usage :", sys.argv[0], " --insert: The maximal insertion sequence length at the TDNA insertion sites. The default value is 3;"
+        print "Usage :", sys.argv[0], " --read_min_REF: The minimal number of mosaic reads bwteen two reference positions. The default value is 4. It can be set to any integer larger or equal to 2. If the program run slow, please increase the number.;"
+        print "Usage :", sys.argv[0], " --read_min_TDNA: The minimal number of mosaic reads required for each TDNA insertion sites as well as mosaic reads between two TDNA positions. The default value is 2. It can be set to any integer larger or equal to 2. ;"
+        print "Usage :", sys.argv[0], " --Mosaic_length: the length of output sequences of each mosaic side;"
+        print "Usage :", sys.argv[0], " --Sample_name: sample names will be used to label output files,defalut is ss;"
+        print "Usage :", sys.argv[0], " --resume: Whether to resume previous run. The default is true;"
+	print "Usage :", sys.argv[0], " -o/--output: The output directory. The default is current directory;"
+	print datetime.datetime.now()
+	print "Author  : Shaofang Li"
+	print "Contact : shaofangli2021@hotmail.com"
+	sys.exit()
+
+def listToString(ss):
+  Str = '';
+  for a in ss:
+    Str += a+' ';
+  return Str;
+
+if (not os.path.exists(output)):
+        os.system("mkdir %s" % output)
+
+### setting up the logging format 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
+                    filename=output+'/log.T_LOC' + str(datetime.datetime.now())+'.txt' ,
+                    filemode='w')
+##### Getting Start Time ######
+if(resume =="true"):
+        logging.debug('Resume the program with [%s]\n', listToString(sys.argv));
+else:
+        logging.debug('Start the program with [%s]\n', listToString(sys.argv));
+startTime = time.time();
+
+#full program would include fastq files, Reference genome files, 
+#get path of the main program
+path = os.path.abspath(os.path.dirname(__file__));
+##get the path  of the python  programs
+bin_path = "%s/bin" % path
+REF_path ="%s/REF" % output 
+TDNA_path ="%s/TDNA" % output
+Result_path = "%s/Result" % output
+Control_path = "%s/Control" % output
+if (not os.path.exists(REF_path)):
+    os.system("mkdir %s" % REF_path)
+if (not os.path.exists(TDNA_path)):
+    os.system("mkdir %s" % TDNA_path)
+if (not os.path.exists(Result_path)):
+    os.system("mkdir %s" % Result_path)
+if (not os.path.exists(Control_path)):
+    os.system("mkdir %s" % Control_path)
+##do the bwa alignment to the reference genome
+if(not bamR):
+	logging.debug("#######################Run fastq alignment with fastq files#######################\n")
+        if(resume=="true" and os.path.exists("%s/Bwa_genome/Done_Bwa_index_genome.txt" %output)):
+            genome_Bwa = "%s/Bwa_genome/%s" %(output, re.sub(".*\\/","",genome))
+	if(not genome_Bwa):
+		cmd1 = "mkdir %s/Bwa_genome" % output
+		cmd2 = "cp %s %s/Bwa_genome/" %(genome, output)
+		cmd3 = "bwa index  %s/Bwa_genome/%s" %(output, re.sub(".*\\/","",genome))
+		logging.debug(cmd1)
+		logging.debug(cmd2)
+		logging.debug(cmd3)
+		os.system(cmd1)
+                os.system(cmd2)
+                os.system(cmd3)
+		genome_Bwa ="%s/Bwa_genome/%s" %(output, re.sub(".*\\/","",genome))
+		logging.debug("Generate the bwa index for the reference genome\n")
+		fw = open("%s/Bwa_genome/Done_Bwa_index_genome.txt" %output,"w")
+                fw.close()
+                logging.debug("Output the file for finishing the indexing the genome\n")
+        if(resume=="true" and os.path.exists("%s/Reference_%s.sorted.bam.bai" %(REF_path, Sample_name))):
+            bamR = "%s/Reference_%s.sorted.bam" % (REF_path,Sample_name) 
+        if(not bamR):
+            cmd_bwa1 = "bwa mem %s %s > %s/Reference_%s.sam 2> %s/Reference_bwa_%s.txt" % (genome_Bwa, re.sub(","," ", fastq), REF_path,Sample_name,REF_path,Sample_name)
+            bamR = "%s/Reference_%s.sorted.bam" % (REF_path,Sample_name)
+            logging.debug(cmd_bwa1)
+	    os.system(cmd_bwa1)
+	    logging.debug("using bwa mem  to do the alignment of reference genome\n")
+            cmd1 = "samtools view -bhST %s %s/Reference_%s.sam -o %s/Reference_%s.bam" % (genome, REF_path,Sample_name, REF_path,Sample_name)
+            cmd2 = "samtools sort -o %s/Reference_%s.sorted.bam %s/Reference_%s.bam"% (REF_path,Sample_name, REF_path,Sample_name)
+            cmd3 = "samtools index %s" % (bamR)
+            logging.debug(cmd1)
+            logging.debug(cmd2)
+            logging.debug(cmd3)
+            os.system(cmd1)
+            os.system(cmd2)
+            os.system(cmd3)
+            logging.debug("using samtools to generate sorted.bam files \n")
+clipped_samR = "%s/Reference_clipped_%s.sam" % (REF_path,Sample_name)
+cmd = "samtools view " + bamR + "|awk '$6~/[S]/' - > " + clipped_samR
+logging.debug(cmd)
+os.system(cmd)
+logging.debug("output reference clipped reads\n")
+##do the bwa alignment to the TDNA
+if(not bamT ):
+    logging.debug("#######################Run fastq alignment with fastq files#######################\n")
+    fq = fastq.split(",")
+    if(not TDNA_Bwa):
+            cmd1 = "mkdir %s/Bwa_TDNA" % output
+            cmd2 = "cp %s %s/Bwa_TDNA/" %(TDNA, output)
+            cmd3 = "bwa index  %s/Bwa_TDNA/%s" %(output, re.sub(".*\\/","",TDNA))
+            logging.debug(cmd1)
+            logging.debug(cmd2)
+            logging.debug(cmd3)
+            os.system(cmd1)
+            os.system(cmd2)
+            os.system(cmd3)
+            TDNA_Bwa ="%s/Bwa_TDNA/%s" %(output, re.sub(".*\\/","",TDNA))
+            logging.debug("Generate the bwa index for the T-DNA sequence\n")
+    if(resume=="true" and os.path.exists("%s/TDNA_%s.sorted.bam.bai" %(TDNA_path, Sample_name))):
+            bamT = "%s/TDNA_%s.sorted.bam" % (TDNA_path,Sample_name)
+    if(not bamT):
+        cmd_bwa = "bwa mem %s %s > %s/TDNA_%s.sam 2> %s/TDNA_bwa_%s.txt" % (TDNA_Bwa,re.sub(","," ", fastq), TDNA_path,Sample_name,TDNA_path,Sample_name)
+        bamT = "%s/TDNA_%s.sorted.bam" % (TDNA_path,Sample_name)
+        logging.debug(cmd_bwa)
+        os.system(cmd_bwa)
+        logging.debug("using bwa mem files to do the alignment of T-DNA\n")
+        cmd1 = "samtools view -hS -F4   %s/TDNA_%s.sam |samtools view -bhST  %s -   -o %s/TDNA_%s.bam" %(TDNA_path,Sample_name,TDNA, TDNA_path,Sample_name)
+        cmd2 = "samtools sort -o %s %s/TDNA_%s.bam"% (bamT, TDNA_path,Sample_name)
+        cmd3 = "samtools index %s" % (bamT)
+        logging.debug(cmd1)
+        logging.debug(cmd2)
+        logging.debug(cmd3)
+        os.system(cmd1)
+        os.system(cmd2)
+        os.system(cmd3)
+        logging.debug("using samtools to generate sorted.bam files \n")
+clipped_samT = "%s/TDNA_clipped_%s.sam" % (TDNA_path,Sample_name)
+cmd = "samtools view " + bamT + "|awk '$6~/[S]/' - > " + clipped_samT
+logging.debug(cmd)
+os.system(cmd)
+logging.debug("output TDNA clipped reads\n")
+if(not control_ID):
+    logging.debug("#######################Generated the control ID  files or skip the calculation on reference clipped files #######################\n")
+if(not control_ID and control_bamR):
+    C_bamR = control_bamR.split(",")
+    clipped_controlR = ""
+    for i in range(0, len(C_bamR)):
+        cmd = "samtools view " + C_bamR[i] + "|awk '$6~/[S]/' - > " + Control_path + "/Control_clipped_" + str(i+1)+ ".sam"
+        logging.debug(cmd)
+        clipped_controlR = clipped_controlR + "," + Control_path + "/Control_clipped_" + str(i+1) + ".sam"
+    clipped_controlR = re.sub(",","", clipped_controlR)
+    control_ID = Control_path + "/Control_id.txt"
+    cmd = "Rscript %s/Clipped_control_REF.R %s %s %s" % (bin_path, clipped_controlR, control_ID, anchor )
+    os.system(cmd)
+    logging.debug("output ID from control\n")
+if( not control_ID and control_fastq):
+    C_fastq = control_fastq.split(":")
+    clipped_controlR = ""
+    for i in range(0, len(C_fastq)):
+        cmd_bwa1 = "bwa mem  %s %s > %s/Reference_control_%s.sam 2> %s/Reference_control_%s_bwa.txt" % (genome_Bwa, re.sub(","," ", C_fastq[i]), Control_path,i+1,Control_path,i+1)
+        cmd = "samtools view " + Control_path + "/Reference_control_"+  str(i +1) + ".sam" + "|awk '$6~/[S]/' - > " + Control_path + "/Control_clipped_" + str(i+1)+ ".sam"
+        logging.debug(cmd_bwa1)
+        os.system(cmd_bwa1)
+        logging.debug(cmd)
+        os.system(cmd)
+        clipped_controlR = clipped_controlR + "," + Control_path + "/Control_clipped_" + str(i+1)+ ".sam"
+        logging.debug(clipped_controlR)
+    clipped_controlR = re.sub(",","", clipped_controlR)
+    control_ID = Control_path + "/Control_id.txt"
+    cmd = "Rscript %s/Clipped_control_REF.R %s %s %s" % (bin_path, clipped_controlR, control_ID, anchor )
+    os.system(cmd)
+    logging.debug("output ID from control\n")
+
+if (control_ID):
+    cmd = "Rscript %s/Clipped_REF.R %s %s %s %s %s/%s_mosaic_REF_TDNA.txt  %s/%s_mosaic_REF_REF.txt  %s/%s_mosaic_all.txt %s %s %s %s" % (bin_path,genome, TDNA, clipped_samR, control_ID, REF_path, Sample_name, REF_path,Sample_name,REF_path,Sample_name, anchor, read_min_TDNA, read_min_REF, insert)
+    logging.debug(cmd)
+    os.system(cmd)
+    logging.debug("output mosaic read from clipped reads in reference\n")
+
+
+cmd = "Rscript %s/Clipped_TDNA.R %s %s %s  %s/%s_mosaic_REF_TDNA.txt  %s/%s_mosaic_TDNA_TDNA.txt  %s/%s_mosaic_all.txt %s %s %s" % (bin_path,genome, TDNA, clipped_samT,TDNA_path, Sample_name,TDNA_path,Sample_name,TDNA_path,Sample_name, anchor, read_min_TDNA,  insert)
+logging.debug(cmd)
+os.system(cmd)
+logging.debug("output mosaic read from clipped reads in TDNA\n")
+
+if(os.path.exists("%s/%s_mosaic_REF_TDNA.txt" % (REF_path, Sample_name) )):
+    cmd = "Rscript %s/Mosaic_output.R  %s %s %s/%s_mosaic_REF_TDNA.txt  %s/Mosaic_%s_FromREF.pdf %s/Mosaic_%s_FromREF.fa %s %s" % (bin_path,genome, TDNA, REF_path,Sample_name,Result_path,Sample_name, Result_path,Sample_name,Sample_name,  Mosaic_length)
+    logging.debug(cmd)
+    os.system(cmd)
+    logging.debug("output Mosaic diagram and fasta files from clipped REF reads\n")
+    cmd = "Rscript %s/Coverage_bam.R  %s/%s_mosaic_REF_TDNA.txt  %s %s/Cov_%s_FromREF.pdf %s %s" % (bin_path,REF_path,Sample_name,bamR, Result_path,Sample_name, REF_path, Sample_name)
+    logging.debug(cmd)
+    os.system(cmd)
+    logging.debug("output coverage for mosaic sites get from clipped REF reads\n")    
+
+cmd = "Rscript %s/Mosaic_output.R  %s %s %s/%s_mosaic_REF_TDNA.txt  %s/Mosaic_%s_FromTDNA.pdf %s/Mosaic_%s_FromTDNA.fa %s %s" % (bin_path,genome, TDNA, TDNA_path,Sample_name,Result_path,Sample_name, Result_path,Sample_name, Sample_name,Mosaic_length)
+logging.debug(cmd)
+os.system(cmd)
+logging.debug("output Mosaic diagram and fasta files from clipped TDNA reads\n")
+cmd = "Rscript %s/Coverage_bam.R  %s/%s_mosaic_REF_TDNA.txt  %s %s/Cov_%s_FromTDNA.pdf %s %s" % (bin_path,TDNA_path,Sample_name,bamR, Result_path,Sample_name, TDNA_path,Sample_name)
+logging.debug(cmd)
+os.system(cmd)
+logging.debug("output coverage for mosaic sites get from clipped TDNA reads\n")   
+
+
+logging.debug("Finish runing T_LOC\n")
